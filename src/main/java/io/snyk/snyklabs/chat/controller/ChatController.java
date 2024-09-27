e391bf7c05849fd5777ab37fd35585b911511fa8
Joe Ruello <joeruello@gmail.com>
2024-09-27 16:10:39 +1000

    wow

--- src/main/java/io/snyk/snyklabs/chat/controller/ChatController.java
+++ src/main/java/io/snyk/snyklabs/chat/controller/ChatController.java
@@ -100,7 +100,6 @@ public class ChatController {
     @GetMapping("/hello")
     public void hello(@RequestParam String user, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-        response.getWriter().write("<h1>Hello: " + user + "</h1>");
         response.getWriter().flush();
     }
 
